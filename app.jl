6603b3270cfd7d0e7824e9caf841e49a9b4438a0
Marc <34219584+mjtkeijsers@users.noreply.github.com>
2021-02-07 18:37:36 +0100

    Update app.jl

--- app.jl
+++ app.jl
@@ -1,13 +1,35 @@
-import Pkg;Pkg.add("HTTP")
+import Pkg;
+Pkg.add("HTTP")
+Pkg.add("Dates")
 
-using HTTP, Sockets
+using HTTP, Sockets, Dates
 
 println("Starting my server ...")
 
-HTTP.listen("127.0.0.1", 8081) do http
+#Write to a file, this file is stored in the dir where
+# the julia program runs
+
+#in case program runs in container data is stored inside container
+# at /app dir as specified WORKDIR in Dockerfile
+open("serverlocalstorage.txt", "a") do io
+       write(io, "Start the server\n")
+   end;
+
+HTTP.listen("0.0.0.0", 80) do http
     println("Caught a request ..." )
+
+    d_now = Dates.format(now(), "HH:MM:SS")
+
+    #if desired can see into the container via
+    # docker exec -t -i mycontainer_id /bin/bash
+    open("serverlocalstorage.txt", "a") do io
+           write(io, "Serving a request\n")
+           write(io, d_now)
+           write(io, "----\n")
+    end;
 
     HTTP.setheader(http, "Content-Type" => "text/html")
     write(http, "MKEJ Hello World of VCP<BR>")
+    write(http, string(d_now))
     return
 end
